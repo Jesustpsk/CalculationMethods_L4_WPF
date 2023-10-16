996b44fc81ca63f72ddd1371b4e45bc56d844f8d
Jesustpsk <dm.kuzmin11@mail.ru>
2023-10-17 00:51:33 +0300

    Initial commit

---
+++ KhaletskyMethod.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculationMethods_L4_WPF;
+
+public static class KhaletskyMethod
+{
+    public static string Khaletsky(double[,] matrix, double[] vector)
+    {
+        
+        return "";
+    }
+}
