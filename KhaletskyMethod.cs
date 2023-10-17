4ba60aae0f55d9e069c67432d78b4df2b6474cce
DmitryKuz <dm.kuzmin11@mail.ru>
2023-10-17 12:02:38 +0300

    fixed KhaletskyMethod.cs

--- KhaletskyMethod.cs
+++ KhaletskyMethod.cs
@@ -4,67 +4,69 @@ namespace CalculationMethods_L4_WPF;
 
 public static class KhaletskyMethod
 {
-    public static string Khaletsky(double[,] A, double[] b)
+
+    public static string Khaletsky(double[,] a_matrix, double[] b_vector, int size)
     {
-        var n = A.GetLength(0);
+        var Lt = new double[size, size]; //Верняя треугольная матрица
+        var L = new double[size, size]; //Нижняя треугольная матрица
+        var y = new double[size]; //Вектор у-ков
+        var x_vector = new double[size]; //Вектор иксов
 
-        var L = new double[n, n];
-        var Lt = new double[n, n];
-
-        // Разложение матрицы A на произведение L и Lt
-        for (var i = 0; i < n; i++)
+        //Находим нижнюю треугольную матрицу 
+        L[0, 0] = Math.Sqrt(a_matrix[0, 0]); //Задаем первый элемент матрицы L как корень из первого элемента матрицы А
+        for (var i = 1; i < size; i++)
         {
-            for (var j = 0; j < (i + 1); j++)
+            var SumJ_El = 0.0;
+            for (var j = 1; j < size + 1; j++)
             {
-                var sum = 0.0;
-                for (var k = 0; k < j; k++)
-                {
-                    sum += L[i, k] * Lt[k, j];
-                }
-
-                if (i == j)
-                {
-                    L[i, j] = Math.Sqrt(A[i, i] - sum);
-                }
-                else
-                {
-                    L[i, j] = (A[i, j] - sum) / L[j, j];
-                }
-
-                Lt[j, i] = L[i, j];
+                if (j - 1 >= i) continue;
+                for (var k = 0; k < j - 1; k++)
+                    SumJ_El += L[i, k] * L[j - 1, k];
+                L[i, j - 1] = (a_matrix[i, j - 1] - SumJ_El) / L[j - 1, j - 1];
             }
+
+            var SumI_El = 0.0;
+            for (var k = 0; k < i; k++)
+            {
+                SumI_El += L[i, k] * L[i, k];
+            }
+
+            L[i, i] = Math.Sqrt(a_matrix[i, i] - SumI_El);
         }
 
-        // Решение системы Ly = b методом прямого хода
-        var y = new double[n];
-        for (var i = 0; i < n; i++)
+        //Транспонируем матрицу L и получаем матрицу LT
+        for (var i = 0; i < size; i++)
+        for (var j = 0; j < size; j++)
+            Lt[i, j] = L[j, i];
+
+        //Рассчитываем вектор у
+        var summa = 0.0;
+        for (var i = 0; i < size; i++)
         {
-            var sum = 0.0;
+            summa = 0.0;
             for (var j = 0; j < i; j++)
             {
-                sum += L[i, j] * y[j];
+                summa += (L[i, j] * y[j]);
             }
-            y[i] = (b[i] - sum) / L[i, i];
+
+            y[i] = (b_vector[i] - summa) / L[i, i];
         }
 
-        // Решение системы Ltx = y методом обратного хода
-        var x = new double[n];
-        for (var i = n - 1; i >= 0; i--)
+        //Наконец то находим значение наших иксов
+        for (var i = size - 1; i >= 0; i--)
         {
-            var sum = 0.0;
-            for (var j = i + 1; j < n; j++)
+            summa = 0;
+            for (var j = size - 1; j > i; j--)
             {
-                sum += Lt[i, j] * x[j];
+                summa += Lt[i, j] * x_vector[j];
             }
-            x[i] = (y[i] - sum) / Lt[i, i];
-        }
 
-        var output = "Solution: ";
-        for (var i = 0; i < n; i++)
-        {
-            output += ("x[{0}] = {1}", i, x[i]) + "\n";
+            x_vector[i] = (y[i] - summa) / Lt[i, i];
         }
-
+        var output = "";
+        for (var i = 0; i < size; i++)
+            output += "x" + (i + 1) + " = " + x_vector[i] + "\n";
         return output;
     }
+
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,10 +24,107 @@ namespace CalculationMethods_L4_WPF
         {
             InitializeComponent();
         }
+        
+        private static int size = 0;
+        private double[,]? initA()
+        {
+             if (TB_b5.Text != "")
+             {
+                 double[,] A =
+                 {
+                     {Convert.ToDouble(TB_A1_1.Text.Replace('.', ',')), Convert.ToDouble(TB_A2_1.Text.Replace('.', ',')),
+                         Convert.ToDouble(TB_A3_1.Text.Replace('.', ',')), Convert.ToDouble(TB_A4_1.Text.Replace('.', ',')),
+                         Convert.ToDouble(TB_A5_1.Text.Replace('.', ','))},
+                     {Convert.ToDouble(TB_A1_2.Text.Replace('.', ',')), Convert.ToDouble(TB_A2_2.Text.Replace('.', ',')),
+                         Convert.ToDouble(TB_A3_2.Text.Replace('.', ',')), Convert.ToDouble(TB_A4_2.Text.Replace('.', ',')),
+                         Convert.ToDouble(TB_A5_2.Text.Replace('.', ','))},
+                     {Convert.ToDouble(TB_A1_3.Text.Replace('.', ',')), Convert.ToDouble(TB_A2_3.Text.Replace('.', ',')),
+                         Convert.ToDouble(TB_A3_3.Text.Replace('.', ',')), Convert.ToDouble(TB_A4_3.Text.Replace('.', ',')), 
+                         Convert.ToDouble(TB_A5_3.Text.Replace('.', ','))},
+                     {Convert.ToDouble(TB_A1_4.Text.Replace('.', ',')), Convert.ToDouble(TB_A2_4.Text.Replace('.', ',')),
+                         Convert.ToDouble(TB_A3_4.Text.Replace('.', ',')), Convert.ToDouble(TB_A4_4.Text.Replace('.', ',')), 
+                         Convert.ToDouble(TB_A5_4.Text.Replace('.', ','))},
+                     {Convert.ToDouble(TB_A1_5.Text.Replace('.', ',')), Convert.ToDouble(TB_A2_5.Text.Replace('.', ',')),
+                         Convert.ToDouble(TB_A3_5.Text.Replace('.', ',')), Convert.ToDouble(TB_A4_5.Text.Replace('.', ',')), 
+                         Convert.ToDouble(TB_A5_5.Text.Replace('.', ','))}
+                 };
+                 size = 5;
+                 return A;
+             }
+             if (TB_b4.Text != "")
+             {
+                 double[,] A =
+                 {
+                     {Convert.ToDouble(TB_A1_1.Text.Replace('.', ',')), Convert.ToDouble(TB_A2_1.Text.Replace('.', ',')),
+                         Convert.ToDouble(TB_A3_1.Text.Replace('.', ',')), Convert.ToDouble(TB_A4_1.Text.Replace('.', ','))},
+                     {Convert.ToDouble(TB_A1_2.Text.Replace('.', ',')), Convert.ToDouble(TB_A2_2.Text.Replace('.', ',')),
+                         Convert.ToDouble(TB_A3_2.Text.Replace('.', ',')), Convert.ToDouble(TB_A4_2.Text.Replace('.', ','))},
+                     {Convert.ToDouble(TB_A1_3.Text.Replace('.', ',')), Convert.ToDouble(TB_A2_3.Text.Replace('.', ',')),
+                         Convert.ToDouble(TB_A3_3.Text.Replace('.', ',')), Convert.ToDouble(TB_A4_3.Text.Replace('.', ','))},
+                     {Convert.ToDouble(TB_A1_4.Text.Replace('.', ',')), Convert.ToDouble(TB_A2_4.Text.Replace('.', ',')),
+                         Convert.ToDouble(TB_A3_4.Text.Replace('.', ',')), Convert.ToDouble(TB_A4_4.Text.Replace('.', ','))}
+                 };
+                 size = 4;
+                 return A;
+             }
+             if (TB_b1.Text != "" && TB_b2.Text != "" && TB_b3.Text != "")
+             {
+                 double[,] A =
+                 {
+                     {Convert.ToDouble(TB_A1_1.Text.Replace('.', ',')), Convert.ToDouble(TB_A2_1.Text.Replace('.', ',')),
+                         Convert.ToDouble(TB_A3_1.Text.Replace('.', ','))},
+                     {Convert.ToDouble(TB_A1_2.Text.Replace('.', ',')), Convert.ToDouble(TB_A2_2.Text.Replace('.', ',')),
+                         Convert.ToDouble(TB_A3_2.Text.Replace('.', ','))},
+                     {Convert.ToDouble(TB_A1_3.Text.Replace('.', ',')), Convert.ToDouble(TB_A2_3.Text.Replace('.', ',')),
+                         Convert.ToDouble(TB_A3_3.Text.Replace('.', ','))}
+                 };
+                 size = 3;
+                 return A;
+             }
+             MessageBox.Show("matrix A is empty", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+             return null;
+        }
 
+        private double[]? initb()
+        {
+            if (TB_b5.Text != "")
+            {
+                double[] b =
+                {
+                    Convert.ToDouble(TB_b1.Text.Replace('.', ',')), Convert.ToDouble(TB_b2.Text.Replace('.', ',')),
+                    Convert.ToDouble(TB_b3.Text.Replace('.', ',')), Convert.ToDouble(TB_b4.Text.Replace('.', ',')),
+                    Convert.ToDouble(TB_b5.Text.Replace('.', ','))
+                };
+                return b;
+            }
+            if (TB_b4.Text != "")
+            {
+                double[] b =
+                {
+                    Convert.ToDouble(TB_b1.Text.Replace('.', ',')), Convert.ToDouble(TB_b2.Text.Replace('.', ',')),
+                    Convert.ToDouble(TB_b3.Text.Replace('.', ',')), Convert.ToDouble(TB_b4.Text.Replace('.', ','))
+                };
+                return b;
+            }
+            if (TB_b1.Text != "" && TB_b2.Text != "" && TB_b3.Text != "")
+            {
+                double[] b =
+                {
+                    Convert.ToDouble(TB_b1.Text.Replace('.', ',')), Convert.ToDouble(TB_b2.Text.Replace('.', ',')),
+                    Convert.ToDouble(TB_b3.Text.Replace('.', ','))
+                };
+                return b;
+            }
+            MessageBox.Show("matrix b is empty", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            return null;
+        }
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            TbOutput.Text = "";
+            var A = initA();
+            var b = initb();
+            if (size == 0) return;
+            TbOutput.Text = KhaletskyMethod.Khaletsky(A, b, size);
         }
     }
 }
