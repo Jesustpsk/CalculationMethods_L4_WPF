f2229a71d982670da7633f8ed44f8276b293c17e
DmitryKuz <dm.kuzmin11@mail.ru>
2023-10-17 10:17:50 +0300

    add Khaletsky Method

--- KhaletskyMethod.cs
+++ KhaletskyMethod.cs
@@ -1,10 +1,70 @@
-﻿namespace CalculationMethods_L4_WPF;
+﻿using System;
+
+namespace CalculationMethods_L4_WPF;
 
 public static class KhaletskyMethod
 {
-    public static string Khaletsky(double[,] matrix, double[] vector)
+    public static string Khaletsky(double[,] A, double[] b)
     {
-        
-        return "";
+        var n = A.GetLength(0);
+
+        var L = new double[n, n];
+        var Lt = new double[n, n];
+
+        // Разложение матрицы A на произведение L и Lt
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = 0; j < (i + 1); j++)
+            {
+                var sum = 0.0;
+                for (var k = 0; k < j; k++)
+                {
+                    sum += L[i, k] * Lt[k, j];
+                }
+
+                if (i == j)
+                {
+                    L[i, j] = Math.Sqrt(A[i, i] - sum);
+                }
+                else
+                {
+                    L[i, j] = (A[i, j] - sum) / L[j, j];
+                }
+
+                Lt[j, i] = L[i, j];
+            }
+        }
+
+        // Решение системы Ly = b методом прямого хода
+        var y = new double[n];
+        for (var i = 0; i < n; i++)
+        {
+            var sum = 0.0;
+            for (var j = 0; j < i; j++)
+            {
+                sum += L[i, j] * y[j];
+            }
+            y[i] = (b[i] - sum) / L[i, i];
+        }
+
+        // Решение системы Ltx = y методом обратного хода
+        var x = new double[n];
+        for (var i = n - 1; i >= 0; i--)
+        {
+            var sum = 0.0;
+            for (var j = i + 1; j < n; j++)
+            {
+                sum += Lt[i, j] * x[j];
+            }
+            x[i] = (y[i] - sum) / Lt[i, i];
+        }
+
+        var output = "Solution: ";
+        for (var i = 0; i < n; i++)
+        {
+            output += ("x[{0}] = {1}", i, x[i]) + "\n";
+        }
+
+        return output;
     }
 }
--- obj/Debug/net7.0-windows/App.g.cs
+++ obj/Debug/net7.0-windows/App.g.cs
@@ -44,7 +44,7 @@ namespace CalculationMethods_L4_WPF {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -59,7 +59,7 @@ namespace CalculationMethods_L4_WPF {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         public static void Main() {
             CalculationMethods_L4_WPF.App app = new CalculationMethods_L4_WPF.App();
             app.InitializeComponent();
